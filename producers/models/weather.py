fcf50a08f7646cee9fb94609c9076ab53224a49e
Gustavo Bragança <ga.braganca@gmail.com>
2021-02-02 18:22:39 -0300

    Fix weather message event

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -78,7 +78,7 @@ class Weather(Producer):
                             "key": {"timestamp": self.time_millis()},
                             "value": {
                                 "temperature": self.temp,
-                                "status": self.status,
+                                "status": self.status.name,
                             },
                         }
                     ],
